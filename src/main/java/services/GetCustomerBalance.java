d713f6acc82f3cb01367b6372fdb3afb98b75c63
vincent VB <vincent.budianto@yahoo.co.id>
2019-11-16 18:39:06 +0700

    feat(GetCustomerBalance): Added get customer balance services

---
+++ src/main/java/services/GetCustomerBalance.java
@@ -0,0 +1,31 @@
+package services;
+import java.sql.*;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.xml.bind.annotation.XmlElement;
+
+@WebService()
+public class GetCustomerBalance {
+    @WebMethod
+    public String GetCustomerBalance(@XmlElement(name = "account") String account) {
+        try {
+            Class.forName("org.mariadb.jdbc.Driver").newInstance();
+            Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/bank_pro","root", "");
+            Statement stmt = conn.createStatement();
+            String query = "select balance from customers where accountNumber = " + account;
+            ResultSet res = stmt.executeQuery(query);
+
+            if (res.next()) {
+                return "200";
+            } else {
+                return "300";
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return "500";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "400";
+        }
+    }
+}
