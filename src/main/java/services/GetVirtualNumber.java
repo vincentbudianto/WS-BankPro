b3eb64093b4f36a39d043b7318ce51dbd37d573b
vincent VB <vincent.budianto@yahoo.co.id>
2019-11-16 22:23:38 +0700

    perf(GetVirtualNumber): Get virtual number services return virtual number data class

--- src/main/java/services/GetVirtualNumber.java
+++ src/main/java/services/GetVirtualNumber.java
@@ -1,4 +1,5 @@
 package services;
+import data.VirtualNumber;
 import java.sql.*;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
@@ -7,8 +8,10 @@ import javax.xml.bind.annotation.XmlElement;
 @WebService()
 public class GetVirtualNumber {
     @WebMethod
-    public String GetVirtualNumber(@XmlElement(name = "senderAccount") String senderAccount,
+    public VirtualNumber GetVirtualNumber(@XmlElement(name = "senderAccount") String senderAccount,
             @XmlElement(name = "receiverAccount") String receiverAccount) {
+        VirtualNumber result = new VirtualNumber();
+
         try {
             Class.forName("org.mariadb.jdbc.Driver").newInstance();
             Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/bank_pro","root", "");
@@ -16,13 +19,16 @@ public class GetVirtualNumber {
             String query = "insert into virtual_accounts (accountNumber, virtualNumber) values (" + receiverAccount + ", " + senderAccount + ")";
             stmt.executeQuery(query);
 
-            return "200";
+            result.setStatus(200);
+            result.setVirtualNumber(senderAccount);
         } catch (SQLException e) {
             e.printStackTrace();
-            return "500";
+            result.setStatus(500);
         } catch (Exception e) {
             e.printStackTrace();
-            return "400";
+            result.setStatus(400);
+        } finally {
+            return result;
         }
     }
 }
