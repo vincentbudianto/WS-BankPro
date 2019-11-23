f8bbc16db63157b7e6895bc090e9527377de09e3
vincent VB <vincent.budianto@yahoo.co.id>
2019-11-24 01:44:32 +0700

    ci(WS-Bank Pro): Fixed path error

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 cd ws-bankpro
 mvn package
-sudo cp /ws-bankpro/target/*.war /opt/tomcat/latest/webapps
+sudo cp target/web_service_bank_pro.war /opt/tomcat/latest/webapps
 sudo systemctl restart tomcat
